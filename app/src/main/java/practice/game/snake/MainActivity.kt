801b68464d8593753ace6fffccdd53776f27ded8
kitbolourchi <74569356+KitBolourchi@users.noreply.github.com>
2023-09-01 11:14:53 +0100

    Progress on snake init

--- app/src/main/java/practice/game/snake/MainActivity.kt
+++ app/src/main/java/practice/game/snake/MainActivity.kt
@@ -10,6 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import practice.game.snake.ui.theme.SnakeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +29,36 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Snake()
+                }
+            }
+        }
+    }
+}
+
+data class State(val food: Pair<Int, Int>, val snake: List<Pair<Int, Int>>)
+
+class Game(scope: CoroutineScope) {
+    private val mutableState =
+        MutableStateFlow(State(food = Pair(5, 5), snake = listOf(Pair(7, 7))))
+
+    val state:Flow<State> = mutableState
+
+    var move = Pair(1, 0)
+
+    init {
+        scope.launch {
+            var snakeLength = 4
+
+            while (true) {
+                delay(150)
+                mutableState.update {
+                    val newPosition = it.snake.first().let { pos ->
+                        Pair(
+                            pos.first + move.first,
+                            pos.second + move.second
+                        )
+                    }
+                    it.copy()
                 }
             }
         }
