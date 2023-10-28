a597f7c0fcd8403ffd247fd11f75745a2bac6601
kitbolourchi <74569356+KitBolourchi@users.noreply.github.com>
2023-10-28 12:24:19 +0100

    Working snake android game

--- app/src/main/java/practice/game/snake/MainActivity.kt
+++ app/src/main/java/practice/game/snake/MainActivity.kt
@@ -8,13 +8,21 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
@@ -26,6 +34,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -42,6 +51,8 @@ import kotlin.random.Random
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val game = Game(lifecycleScope)
         setContent {
             SnakeTheme {
                 // A surface container using the 'background' color from the theme
@@ -49,7 +60,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-//                    Snake()
+                    Snake(game)
                 }
             }
         }
@@ -122,17 +133,31 @@ fun Snake(game: Game) {
         state.value?.let {
             Board(it)
         }
-        Buttons()
+        Buttons {
+            game.move = it
+        }
     }
 
 }
 
 @Composable
-fun Buttons(onDirectionChange: (Pair<Int, Int>)-> Unit) {
+fun Buttons(onDirectionChange: (Pair<Int, Int>) -> Unit) {
     val buttonSize = Modifier.size(64.dp)
-    Column(horizontalAlignment =  Alignment.CenterHorizontally, modifier = Modifier.padding(24.dp)) {
-        Button(onClick = { onDirectionChange }) {
-            
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(24.dp)) {
+        Button(onClick = { onDirectionChange(Pair(0, -1)) }, modifier = buttonSize) {
+            Icon(Icons.Default.KeyboardArrowUp, null)
+        }
+        Row {
+            Button(onClick = { onDirectionChange(Pair(-1, 0)) }, modifier = buttonSize) {
+                Icon(Icons.Default.KeyboardArrowLeft, null)
+            }
+            Spacer(modifier = buttonSize)
+            Button(onClick = { onDirectionChange(Pair(1, 0)) }, modifier = buttonSize) {
+                Icon(Icons.Default.KeyboardArrowRight, null)
+            }
+        }
+        Button(onClick = { onDirectionChange(Pair(0, 1)) }, modifier = buttonSize) {
+            Icon(Icons.Default.KeyboardArrowDown, null)
         }
     }
 }
@@ -171,5 +196,7 @@ fun Board(state: State) {
 @Preview(showBackground = true)
 @Composable
 fun SnakeGamePreview() {
-//    Snake()
+    SnakeTheme {
+
+    }
 }
